c82cdcd47bc2b7caa00d893892d8c6cc8d516129
mkalansari7 <mk.alansari7@gmail.com>
2022-01-09 18:44:57 +0300

    done

--- challenge1.js
+++ challenge1.js
@@ -29,3 +29,45 @@
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+function printName() {
+    console.log("Mohammad Alansari");
+}
+
+printName();
+
+function printAge(birthYear) {
+    age = (2022 - birthYear);
+    console.log(age);
+}
+
+printAge(1996);
+
+function printHello(name, language) {
+    if (language === "en"){
+    console.log(`Hello ${name}`);
+    
+    } else if (language === "es") {
+        console.log(`Hola ${name}`);
+    } else if (language === "fr") {
+        console.log(`Bonjour ${name}`);
+    } else if (language === "tr") {
+        console.log(`Merhaba ${name}`);
+    } else {
+        console.log("Please choose another language");
+    }
+
+}
+
+printHello("Mohammad", "es");
+
+function printMax(firstNum, secondNum) {
+    if (firstNum > secondNum) {
+        console.log(`The bigger number is ${firstNum}`);
+    } else if (secondNum > firstNum) {
+        console.log(`The bigger number is ${secondNum}`); 
+    } else {
+        console.log("the numbers are equal");
+    }
+}
+
+printMax(36, 20);
